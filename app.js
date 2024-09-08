88fec825ac4bda3601bd12c2e3f7b2f710e05439
alesyfrontend <alesy2007@gmail.com>
2024-09-07 23:55:04 -0300

    imersaoalura

---
+++ app.js
@@ -0,0 +1,42 @@
+
+function pesquisar(){
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+    let campoPesquisa=document.getElementById("campo-pesquisa").value
+    //console.log(campoPesquisa);
+   
+    // se o campo pesquisa for uma string sem nada
+    if (campoPesquisa == ""){
+        section.innerHTML= "<h1> Favor digitar a raça do cachorro desejado </h1>"
+        return
+    }
+    campoPesquisa =campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+  
+    // Itera sobre cada dado e cria o HTML correspondente
+    for (let dado of dados) {
+        titulo=dado.titulo.toLowerCase()
+        descricao= dado.descricao.toLowerCase()
+        
+
+        if (titulo.includes (campoPesquisa) || descricao.includes(campoPesquisa)){
+            resultados += `
+            <div class="item-resultado"> <h2> <a href="#" target="_blank"> ${dado.titulo}</a> 
+            </h2>
+            <p class="descricao-meta"> ${dado.descricao} </p>
+            <a href=${dado.link} target="_blank"> Mais informações</a> </div>
+            `;
+        }
+  
+    }
+           
+        
+      
+    // Insere os resultados na seção HTML
+    section.innerHTML = resultados;
+  }
---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados= [
+{
+    titulo:"Rayssa Leal",
+    descricao: "Rayssa Leal, conhecida carinhosamente como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, já demonstrava uma habilidade impressionante sobre o skate, chamando a atenção de todos com suas manobras e estilo único.",
+    link:"https://pt.wikipedia.org/wiki/Rayssa_Leal"
+},
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua trajetória inspiradora e sua luta por mais representatividade no esporte a tornaram um ícone.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+ },
+
+{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua habilidade, determinação e carisma. Bicampeã olímpica, ela é considerada a maior medalhista olímpica da história do Brasil.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+}
+];
+
+
+
+
