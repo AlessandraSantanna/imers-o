1ea27362a45a688eace2b083bd1c4386717f6a15
alesyfrontend <alesy2007@gmail.com>
2024-09-09 10:25:28 -0300

    inclusao caes

--- dados.js
+++ dados.js
@@ -3,41 +3,86 @@ let dados= [
     titulo:"Shih Tzu",
     descricao: "O Shih Tzu é uma raça chinesa, derivada dos Lhasa Apso. <br> É um cão de porte pequeno, com 20 a 26 cm de altura, pesando de 4 a 8 kg. São conhecidos por serem uma raça doce e porque gostam muito de serem mimados. Por isso, são frequentemente escolhidos para serem o cão de companhia da família. <br> Sua pelagem longa exige bastante cuidado – escovação diária e tosa nas épocas mais quentes. Os cachorros dessa raça convivem bem com outros cachorros e com crianças. <br> A média de vida de um Shih Tzu é de 10 a 18 anos.<br> Por não precisar de muito espaço para se exercitar, é um bom cachorro para viver em apartamento.",
     link:"https://pt.wikipedia.org/wiki/Shih_Tzu",
-    imagem: "shihtzunsc.jpg" 
+    imagem: "./imagens/shihtzunsc.jpg" 
    
 },
 {
     titulo: "Poodle",
     descricao: "O Poodle é uma das raças de cachorro mais populares do país e do mundo. <br> SExistem grandes variedades de cores de poodles e 4 variedades de tamanho (do grande até o poodle toy, os poodles podem ter de 25 a 60 cm de altura).<br>A vida média de um poodle é de 14 a 18 anos. Os poodles grandes, por terem mais energia, precisam de locais maiores para viverem – o que não é um problema para os poodles toys, que vivem bem em lugares pequenos.",
     link: "https://pt.wikipedia.org/wiki/Poodle",
-    imagem: "poodle_cachorro.png" 
+    imagem: "./imagens/poodle_cachorro.png" 
  },
 
 {
     titulo: "Yorkshire",
-    descricao: "Essa raça de cachorros muito pequenos faz muito sucesso entre as famílias. Antigamente, eram usados para a caça de roedores, mas hoje costumam ser adotados como um cachorro de companhia, pois não são cachorros adequados para viver fora de casa. <br>Os Yorkshires são cachorros muito meigos e inteligentes, geralmente se apegam muito aos seus donos e costumam latir para estranhos. <br>Seu pequeno porte (de 20 a 22 cm de altura e pesando, em média, 4 kg) faz com que o Yorkshire não seja muito adequado para casas com crianças pequenas, por terem corpo frágil. Eles vivem, em média, de 12 a 16 anos.<br> Devido ao seu grande porte (de 55 a 65 cm de altura, podendo pesar até 40 kg), é necessário cuidado com a displasia coxofemoral, doença frequente em raças como essa.",
+    descricao: "Essa raça de cachorros muito pequenos faz muito sucesso entre as famílias. Antigamente, eram usados para a caça de roedores, mas hoje costumam ser adotados como um cachorro de companhia, pois não são cachorros adequados para viver fora de casa. <br>Os Yorkshires são cachorros muito meigos e inteligentes, geralmente se apegam muito aos seus donos e costumam latir para estranhos. <br>Seu pequeno porte (de 20 a 22 cm de altura e pesando, em média, 4 kg) faz com que o Yorkshire não seja muito adequado para casas com crianças pequenas, por terem corpo frágil. Eles vivem, em média, de 12 a 16 anos.",
     link: "https://pt.wikipedia.org/wiki/Yorkshire_terrier",
-    imagem: "Yorkshire.jpg" 
+    imagem: "./imagens/Yorkshire.jpg" 
 },
 {
     titulo: "Pastor Alemão",
     descricao: "O pastor alemão é uma raça que ficou conhecida como cão de guarda, policial ou militar. <br>Seu faro é muito bom, por isso, é uma raça muito usada por policiais e bombeiros para rastrear drogas ou até mesmo fazer resgate de pessoas perdidas.",
     link: "https://pt.wikipedia.org/wiki/Pastor-alemão",
-    imagem: "pastor.jpg" 
+    imagem: "./imagens/pastor.jpg" 
 },
 {
     titulo: " Labrador",
     descricao: "O labrador é uma raça de cachorro muito simpática, que é muito utilizada como cão-guia para cegos. Muito doce, o labrador é um cachorro recomendado para famílias com crianças, pois é um companheiro de ótimo temperamento. <br>.É uma raça muito ativa, que gosta de esporte e de nadar. É preciso fazer exercícios para que o cão possa direcionar sua energia, para não causar problemas como a mastigação e a destruição de alguns pertences.<br>Os labradores são cães de grande porte (tendo de 54 a 57 cm de altura e podendo pesar mais de 30 kg) e vivem, em média, de 10 a 13 anos.",
     link: "https://pt.wikipedia.org/wiki/Labrador_retrievero",
-    imagem: "labrador.jpg" 
+    imagem: "./imagens/labrador.jpg" 
 },
 {
     titulo: "  Dalmata",
     descricao: "O Dálmata é um nato cão de caça, mas, ainda assim, é sensível, calmo, inteligente e sem nenhuma timidez. O seu corpo é dotado de manchas pretas características da raça, além de ser musculoso. É considerado uma das raças mais conhecidas do planeta e é um ótimo cão-guia para cegos e pessoas com autismo.<br>De fato, o Dálmata é uma raça encantadora, se tornando inesquecível na vida de qualquer tutor que o tem. Uma das características mais marcantes desse cão, com exceção das pintas, é a sua personalidade.",
     link: "https://pt.wikipedia.org/wiki/D%C3%A1lmata_(c%C3%A3o)",
-    imagem: "dalmata.jpg" 
+    imagem: "./imagens/dalmata.jpg" 
+},
+{
+    titulo: " Pitbull terrier",
+    descricao:"  O pit bull atua hoje principalmente como cão de companhia e cão atleta em esportes lícitos, e, em menor escala também como cão de agarre em caça lícita de grandes animais como javalis. cão-guia para cegos. Muito doce, o labrador é um cachorro recomendado para famílias com crianças, pois é um companheiro de ótimo temperamento. <br>.É uma raça muito ativa, que gosta de esporte e de nadar. É preciso fazer exercícios para que o cão possa direcionar sua energia, para não causar problemas como a mastigação e a destruição de alguns pertences.<br>.O pit bull herdou o nome terrier dos seus ancestrais de tipo terrier, que cruzados com o antigo buldogue inglês resultaram numa simbiose perfeita entre força e agilidade, originando o bull-&-terrier, considerado o gladiador das arenas do mundo canino e ancestral direto do pit bull.",
+    link: "https://pt.wikipedia.org/wiki/American_pit_bull_terrier",
+    imagem: "./imagens/pitbull.webp" 
+},
+{
+    titulo: "  Husky Siberiano",
+    descricao: "Muito independente e expansivo, o Husky Siberiano é muito conhecido pelos seus olhos azuis e sua inteligência. No entanto, é um cão difícil de ser adestrado, já que seu temperamento também depende muito da forma como foi criado desde muito pequeno.Ainda assim, o cão é muito companheiro e adora estar em família. Quando o tutor sabe entender sua personalidade e o cria de maneira correta, se torna brincalhão e extremamente dócil com todos àqueles que o cercam.<br> No entanto, é uma raça que nem sempre é indicada para ser tida como um cão de guarda. <br>O motivo é que o Husky é um cão que mesmo assustando com seu porte e aparência imponente, nem sempre terá a reação desejada para proteger o ambiente em questão.",
+    link: "https://pt.wikipedia.org/wiki/D%C3%A1lmata_(c%C3%A3o)",
+    imagem: "./imagens/huskysiberiano.jpg" 
+},
+{
+    titulo: "Vira-lata",
+    descricao: "Os cães popularmente conhecidos como vira-latas possuem algumas vantagens e peculiaridades. Por ser uma mistura de raças, o termo correto é SRD (sem raça definida) e cada pet destes é único, devido a sua combinação de genes e mistura de raças ser totalmente exclusiva. “Não existe um padrão. O vira-lata é a ausência de raça pura, e a origem pode ser qualquer mistura, entre qualquer raça ou entre outros vira-latas. Cada um carrega uma carga genética única.",
+    link: "https://pt.wikipedia.org/wiki/Vira-lata",
+    imagem: "./imagens/viralata.webp" 
+},
+{
+    titulo: "Rottweiler",
+    descricao: "O rottweiler provavelmente descende de cães romanos (provavelmente o cão Molossus) e seus descendentes, que eram criados como cães de guarda e guardiões de rebanho. Esses cães imigraram com as legiões romanas através dos Alpes suíços, até chegarem nos arredores da Floresta negra no sudoeste da Alemanha, onde tiveram contato com os cães da região. Houve, então, uma miscigenação com os cães locais, dando origem à versão primitiva do que hoje chamamos de rottweiler..",
+    link: "https://pt.wikipedia.org/wiki/Rottweiler",
+    imagem: "./imagens/rottweile.jpg" 
+},
+{
+    titulo: "Buldogue",
+    descricao: "Sua origem não é muito certa, embora se saiba que até meados do século XVIII seus ancestrais (possíveis descendentes do alão),<br> mais altos e ágeis, eram utilizados em combates contra touros, de onde herdaram a nomeação 'bulldog'. Quando estes combates (bull-baiting) foram proibidos em 1835, o buldogue quase chegou a extinção.[2] Por volta de 1860,<br> quando as exposições de cães se tornaram populares, o antigo buldogue foi modificado, tornando-se bem mais baixo, com peito mais largo, e focinho achatado com prognatismo mais presente. Algumas fontes relatam que esta mudança de tamanho e modificação do focinho ocorreu devido aos cruzamentos com pug.",
+    link: "https://pt.wikipedia.org/wiki/Buldogue",
+    imagem: "./imagens/buldogue.jpg" 
+},
+{
+    titulo: "Chow-chow",
+    descricao: "É uma raça antiga e de origem ainda desconhecida. Análises de DNA sugerem que estes cães tenham surgido originalmente na Mongólia ou Sibéria, e então foram levados para a China.Na China estes cães são chamados de Songshi Quan (Pinyin: 鬆獅犬 [sōngshī quǎn]), que significa literalmente 'cão-leão-empolado'. <br> É suposto que sua imagem já tenha sido usada para decoração de objetos na Dinastia Han em 206 a.c e que durante a Dinastia Tang um dos imperadores teve em seu canil dois mil e quinhentos exemplares destes caninos, e estes ficaram conhecidos como Tang Quan (cão da Dinastia Tang).",
+    link: "https://pt.wikipedia.org/wiki/Chow-chow",
+    imagem: "./imagens/chowchow.webp" 
 }
 ];
+
+
+
+
+
+
+
+
+
 
 
 
